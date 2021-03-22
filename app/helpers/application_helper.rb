bdbaeb9a14163ea796d3c1bddfcb6828b721569d
ksilex <negative074@gmail.com>
2021-03-22 23:38:03 +0300

    gist formatter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,13 +5,4 @@ module ApplicationHelper
       alert: "alert alert-warning alert-dismissible fade show",
       notice: "alert alert-info alert-dismissible fade show" }.stringify_keys[level.to_s]
   end
-
-  def gist?(link)
-    link[:url].include?('gist.github.com')
-  end
-
-  def gist(link)
-    @client ||= Octokit::Client.new(access_token: '352f1f578c4da9a7bc84eaf6df049a4a073ee058')
-    response = @client.gist(link.url.split('/').last)
-  end
 end
--- app/javascript/scripts/gist_formatter.js
+++ app/javascript/scripts/gist_formatter.js
@@ -1,19 +1,20 @@
 
 import { Octokit } from "@octokit/rest"
 document.addEventListener("turbolinks:load", function() {
-const octokit = new Octokit({
-  auth: "352f1f578c4da9a7bc84eaf6df049a4a073ee058",
-});
-      $('.link').each(function() {
-        if ($(this).children().attr('href').includes('gist.github.com')){
-          var link = this
-          var gist_array = $(this).children().attr('href').split('/')
-          var gist_id = gist_array[gist_array.length - 1]
-          var gist_content = octokit.gists.get({
-            gist_id
-          })
-          .then(res => console.log(res.data.files(Object.keys(res.data.files)[0])))
-        }
-
+  const octokit = new Octokit()
+  
+  $('.link').each(function() {
+    if ($(this).children().attr('href').includes('gist.github.com')){
+      var link = this
+      var gist_array = $(this).children().attr('href').split('/')
+      var gist_id = gist_array[gist_array.length - 1]
+      octokit.gists.get({
+        gist_id
       })
+      .then(res => Object.entries(res.data.files).forEach(
+        ([key, file]) => $(link).append(`<h5>Gist content:</h5> ${file.filename}: ${file.content}`)
+      ))
+    }
+
   })
+})
