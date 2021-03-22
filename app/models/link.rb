59e668b68afc7c4509e41cc2a3d72d75461added
ksilex <negative074@gmail.com>
2021-03-22 20:53:19 +0300

    validations

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,7 @@
 class Link < ApplicationRecord
   belongs_to :parent, polymorphic: true
   validates :name, :url, presence: true
+  validates :url,
+            format: { with: /\A(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?\z/ix,
+            message: 'Invalid url format' }
 end
