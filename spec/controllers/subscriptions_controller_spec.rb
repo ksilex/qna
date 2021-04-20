ae43e240c814684969fe348667db3bedd38ba6c5
ksilex <negative074@gmail.com>
2021-04-20 12:44:37 +0300

    minor

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe SubscriptionsController, type: :controller do
   let!(:question) { create(:question) }
   let(:user) { create(:user) }
 
-  describe 'POST #subscribe_to_question' do
+  describe 'POST #create' do
     before { login(user) }
     it 'saves a new subscription in database' do
       expect { post :create, params: { question_id: question.id } }.to change(user.subscriptions, :count).by(1)
     end
   end
 
-  describe 'DELETE #unsubscribe_from_question' do
+  describe 'DELETE #destroy' do
     let!(:subscription) { create(:subscription, user: user, question: question) }
     before { login(user) }
     it 'deletes subscription from database' do
