209d33aed8a49d9c8aaf588e11269dbb233ac772
ksilex <negative074@gmail.com>
2021-04-27 08:49:49 +0300

    miniracer

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,6 +3,7 @@ lock "~> 3.16.0"
 
 set :application, "qna"
 set :repo_url, "git://github.com/ksilex/qna.git"
+set :bundle_flags, "--deployment"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
